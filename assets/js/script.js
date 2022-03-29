459e004586714916a044a5dfc8e029538f8ade1a
masontmorris <masonmorris8@gmail.com>
2022-03-28 21:26:54 -0400

    makes call to foursquare places api with lat/lng of each venue

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,6 +79,26 @@ function displayEvents(data) {
 
         let eventContainer = $("#event-search-results");
         eventContainer.append(eventCard);
+        testFS(venueLat, venueLng);
+    }
+}
+
+async function testFS(lat, lng) {
+    let fsBaseURL = "https://api.foursquare.com/v3/places/search";
+    let fsAPIKey = "fsq33tA/HPjKRDhV2MuuWp+nKpzNssXSc9zq7A7NH+Qrx30=";
+    let fsQuery = `?ll=${lat},${lng}&radius=500&limit=10`;
+    try {
+        let response = await fetch(fsBaseURL + fsQuery, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: fsAPIKey,
+            },
+        });
+        let data = await response.json();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
     }
 }
 
